b008537a30f15263783537e564cee310fd82caaa
Daniel Watts <thisisruddy@gmail.com>
2022-05-07 10:39:09 +0100

    Added updateCacheNodeTypes call before call to summaryOfNodes

--- scripts/summaryOfNodes.js
+++ scripts/summaryOfNodes.js
@@ -1,7 +1,9 @@
 const nodesData = require("../data/nodeTypesById.json");
 const summariseNodes = require("../lib/summariseNodes");
+const updateCachedNodeTypes = require("./updateCachedNodeTypes");
 
 const execute = async () => {
+  await updateCachedNodeTypes();
   const nodes = Object.values(nodesData);
   const summary = summariseNodes(nodes);
   console.info(`Successfully summarised node counts:`);
