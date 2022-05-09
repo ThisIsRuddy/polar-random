c5c3c4bde9b2c038d779736ddc55acbdabe750fe
Daniel Watts <thisisruddy@gmail.com>
2022-05-09 15:47:28 +0100

    Reworked cache function

--- scripts/updateCachedNodeTypes.js
+++ scripts/updateCachedNodeTypes.js
@@ -1,68 +1,70 @@
-const path = require('path');
 const {PromisePool} = require('@supercharge/promise-pool');
 
 const writeFile = require('../lib/writeFile');
-const cachedEntries = require('../data/nodeTypesById.json');
 const getNodeType = require('../requests/getNodeType');
 const getNodeSpeciality = require('../requests/getNodeSpeciality');
 const getNodeTotalCount = require('../requests/getNodeTotalCount');
 const cachedNodes = require('../data/nodeTypesById.json');
 
+const getMissingIds = (latestId) => {
+  const cachedIds = Object.keys(cachedNodes);
+  const allIds = Array.from(Array(latestId + 1).keys()).map(n => n.toString());
+
+  const missingIds = allIds.filter(id => !cachedIds.includes(id));
+  console.info(`Found ${missingIds.length} missing node types.`);
+
+  return missingIds;
+};
+
+const cacheNodes = async (latestId) => {
+  const ids = getMissingIds(latestId);
+  console.info(`Fetching the type & speciality for ${ids.length} nodes...`);
+
+  const entries = [];
+  await PromisePool
+    .for(ids)
+    .withConcurrency(10)
+    .handleError(async (err, id, pool) => console.error(`[${id}] ${err.message}`))
+    .process(async (id, i, pool) => {
+      const type = await getNodeType(id);
+      const specialty = await getNodeSpeciality(id);
+      entries.push(id);
+      cachedNodes[id] = {
+        id,
+        type: specialty ? `${type} ${specialty}` : type,
+        special: specialty
+      };
+    });
+
+  console.info(`Successfully fetched ${entries.length} nodes.`);
+  return entries;
+}
+
 const updateCachedNodeTypes = async () => {
 
   const cachedKeys = Object.keys(cachedNodes);
   const cachedCount = cachedKeys.length;
-  const lastCachedId = parseInt(cachedKeys[cachedCount - 1]);
+  const cachedLastId = parseInt(cachedKeys[cachedCount - 1] ? cachedKeys[cachedCount - 1] : 0);
+  const latestId = await getNodeTotalCount() + 216; //Hacky fix
+  console.info(`Last node cached was #${cachedLastId}, latestNodeId is #${latestId}.`);
 
-  const latestId = await getNodeTotalCount() + 214; //Hacky fix
-  const updateCount = latestId - lastCachedId;
-  console.info(`Last node cached was #${lastCachedId}, latestNodeId is #${latestId}.`);
-
-  if(!updateCount){
+  if (cachedLastId === latestId) {
     console.info("Cached node types & specialities are up-to date.");
     return;
   }
 
-  const ids = Array.from({length: updateCount}, (v, k) => lastCachedId + (k + 1));
-  const newEntries = [];
-  console.info(`Fetching the type & speciality for ${updateCount} new nodes...`);
+  const newEntries = await cacheNodes(latestId);
+  const retriedEntries = await cacheNodes(latestId);
 
-  await PromisePool
-    .for(ids)
-    .withConcurrency(50)
-    .handleError(async (err, id, pool) => {
-      console.error(`Failed to load types for node ${id}.`);
-      console.error(err.message);
-    })
-    .process(async (id, i, pool) => {
-      console.info(`[${i}] Processing id: ${id}...`);
-      const type = await getNodeType(id);
-      const specialty = await getNodeSpeciality(id);
+  await writeFile('../data/nodeTypesById.json', JSON.stringify(cachedNodes, null, 2));
 
-      const record = {
-        id,
-        type: specialty ? `${type} ${specialty}` : type,
-        special: specialty
-      }
-
-      newEntries[id] = id;
-      cachedEntries[id] = record;
-    });
-
-  try {
-    await writeFile(path.resolve(__dirname, '../data/nodeTypesById.json'), JSON.stringify(cachedEntries, null, 2));
-  } catch (e) {
-    console.error(`Error storing newEntries to file ../data/nodeTypesById.json:`);
-    console.error(e.message);
-  }
-
-  return Object.values(newEntries);
+  return Object.assign({}, newEntries, retriedEntries);
 }
 
 const execute = async () => {
-  const newEntries = await updateCachedNodeTypes();
-  if(newEntries)
-    console.info(`Successfully cached ${newEntries.length} new node types & specialities.`);
+  const entries = await updateCachedNodeTypes();
+  if (entries)
+    console.info(`Successfully cached ${entries.length} new node types & specialities.`);
 }
 
 module.exports = execute;
