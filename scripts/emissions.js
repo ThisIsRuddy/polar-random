b0cb4bfda211c81b94ae6f0f39e000d1e2510c75
Daniel Watts <thisisruddy@gmail.com>
2022-05-15 22:18:23 +0100

    Added emissions functions, restructure commands

---
+++ scripts/emissions.js
@@ -0,0 +1,16 @@
+const updateCache = require('./cache');
+const nodesData = require("../data/nodeTypesById.json");
+const getNodeCountSummary = require("../lib/getNodeCountSummary");
+const getNodeEmissionsSummary = require("../lib/getNodeEmissionsSummary");
+
+const execute = async () => {
+  await updateCache(false);
+  console.info(`Generating node emissions summary...`);
+  const nodes = Object.values(nodesData);
+  const counts = getNodeCountSummary(nodes);
+  const summary = getNodeEmissionsSummary(counts);
+  console.info(`Successfully summarised node emissions:`);
+  console.info(JSON.stringify(summary, null, 2));
+}
+
+module.exports = execute;
